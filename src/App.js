5c2d58ca4a5d345bd2a3cbfaaedbed1e83ec3903
richardleung1 <richardl57549@gmail.com>
2020-12-09 10:50:33 -0500

    increment & decrement buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      count: 0,
+    }
+  }
+
+  increment = () => {
+    const currentCount = this.state.count
+    const newCount = currentCount + 1
+    // this.state = {
+    //   count: newCount
+    // }
+
+    this.setState({ count: newCount })
+  }
+
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 })
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
+      </div>
+    )
+  }
 }
+
 
 export default App;
